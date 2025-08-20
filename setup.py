2cb616344fbbfb11e68741d392bd372253ea8c52
Gabriel Gress <gabriel.gress@live.fr>
2025-08-20 10:30:26 +0900

    Project setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+setup(name='python-parallelism',
+      version="0.0.1",
+      description="Image preprocessing and model training using parallelism in python",
+      license="MIT",
+      author="G-Gress",
+      author_email="gabrielgress.ds@gmail.com",
+      #url="https://github.com/G-Gress/python_parallelism",
+      install_requires=requirements,
+      packages=find_packages(),
+      include_package_data=True)
