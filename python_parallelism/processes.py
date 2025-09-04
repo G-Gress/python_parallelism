210a97a94eea17838089683ed38f8ad89e1a167d
Gabriel Gress <gabriel.gress@live.fr>
2025-09-04 17:05:39 +0900

    Improved docstrings

--- python_parallelism/processes.py
+++ python_parallelism/processes.py
@@ -10,6 +10,13 @@ def batch_worker(batch: List[Any], func: Callable, *args, **kwargs) -> List[Any]
     Applies a function to each item of a batch of data.
     Used as a worker function for parallel processing.
     Returns a list of results.
+
+    Args:
+        batch (List[Any]): The batch of data to process.
+        func (Callable): The function to apply to each item in the batch.
+
+    Returns:
+        List[Any]: List containing the transformed data.
     """
     return [func(item, *args, **kwargs) for item in batch]
 
@@ -26,6 +33,19 @@ def parallel_map_batched(
     """
     Applies a function to a list of paths, splitted into batches.
     Returns a list of results.
+
+    Args:
+        paths (List[Any]): The list of paths to process.
+        func (Callable): The function to apply to each path.
+        batch_size (int, optional): The size of each batch. Defaults to 16.
+        max_workers (int, optional): The maximum number of worker processes to use. Defaults to None.
+        func_args (tuple, optional): Positional arguments to pass to the function. Defaults to ().
+        func_kwargs (dict, optional): Keyword arguments to pass to the function. Defaults to {}.
+        limit (int, optional): The maximum number of paths to process. Defaults to None.
+        display_progress (bool, optional): Whether to display progress information. Defaults to False.
+
+    Returns:
+        List[Any]: The results of applying the function to each batch of paths.
     """
 
     # Reduces the list of paths to a given limit to avoid processing too many files
