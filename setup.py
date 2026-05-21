e75bf55984879fc6bcabcc2531f35ced225b68c0
Gabriel Gress <gabriel.gress@live.fr>
2026-05-21 16:11:28 +0900

    Deleted version and fixed description

--- setup.py
+++ setup.py
@@ -5,13 +5,19 @@ with open("requirements.txt") as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if "git+" not in x]
 
-setup(name='python_parallelism',
-      version="0.0.1",
-      description="Image preprocessing and model training using parallelism in python",
-      license="MIT",
-      author="G-Gress",
-      author_email="gabrielgress.ds@gmail.com",
-      #url="https://github.com/G-Gress/python_parallelism",
-      install_requires=requirements,
-      packages=find_packages(),
-      include_package_data=True)
+setup(
+    name="python_parallelism",
+    description=(
+        "Hands-on learning project: Python parallelism patterns benchmarked "
+        "on computer-vision tasks (image preprocessing + feature extraction)."
+    ),
+    license="MIT",
+    author="G-Gress",
+    author_email="gabrielgress.ds@gmail.com",
+    # url="https://github.com/G-Gress/python_parallelism",
+    install_requires=requirements,
+    packages=find_packages(),
+    include_package_data=True,
+    use_scm_version={"fallback_version": "0.0.0"},
+    setup_requires=["setuptools_scm"],
+)
